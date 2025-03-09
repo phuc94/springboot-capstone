fde121d8298b37e4ae88754fd811fa0e66ab82a7
Nguyễn Phúc <trungphuc94@gmail.com>
2025-03-09 20:19:01 +0700

    add /api to todo controller

--- springboot/src/main/java/com/cybersoft/capstone/controller/TodoController.java
+++ springboot/src/main/java/com/cybersoft/capstone/controller/TodoController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/todo")
+@RequestMapping("/api/todo")
 public class TodoController {
 
     private final TodoService todoService;
