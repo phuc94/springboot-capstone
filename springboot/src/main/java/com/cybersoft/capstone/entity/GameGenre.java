09c8634438d9bd14c7d4c04998ca1714b920aa90
nghiadeveloper <hieunghia.dev@gmail.com>
2025-03-12 14:26:41 +0700

    Hoàn thành task SBC 10

--- springboot/src/main/java/com/cybersoft/capstone/entity/GameGenre.java
+++ springboot/src/main/java/com/cybersoft/capstone/entity/GameGenre.java
@@ -1,17 +1,25 @@
 package com.cybersoft.capstone.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
 
 import java.io.Serializable;
 
-@Entity
+@Entity(name = "game_genre")
+@Table(name = "game_genre", uniqueConstraints = {
+    @UniqueConstraint(columnNames = {"genre_id", "game_id"})
+})
 @Data
 public class GameGenre implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
+    @ManyToOne
+    @JoinColumn(name = "genre_id", referencedColumnName = "id")
+    private Genres genres;
+
+    @ManyToOne
+    @JoinColumn(name = "game_id", referencedColumnName = "id")
+    private Games games;
 }
