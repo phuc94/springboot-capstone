ccbc72942a202e79c0d63b4331828c9af8fa768d
Ngoc-Hoan <hoannguyen91234@gmail.com>
2025-03-14 20:30:53 +0700

    SBC-9

--- springboot/src/main/java/com/cybersoft/capstone/entity/GameSupportLanguage.java
+++ springboot/src/main/java/com/cybersoft/capstone/entity/GameSupportLanguage.java
@@ -7,11 +7,25 @@ import jakarta.persistence.Id;
 import lombok.Data;
 
 import java.io.Serializable;
+import jakarta.persistence.*;
+
 
 @Entity
+@Table(name = "game_support_language", uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"support_language_id", "game_id"})
+})
 @Data
 public class GameSupportLanguage implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private int id;
+
+    @ManyToOne
+    @JoinColumn(name = "support_language_id", nullable = false)
+    private SupportLanguage supportLanguage;
+
+    @ManyToOne
+    @JoinColumn(name = "game_id", nullable = false)
+    private Games game;
 }
