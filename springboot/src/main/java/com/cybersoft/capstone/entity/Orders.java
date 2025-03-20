82b76394c4e44dab49d157f6ac5b34c3762e9751
Ngoc-Hoan <hoannguyen91234@gmail.com>
2025-03-21 06:40:32 +0700

    SBC-19

--- springboot/src/main/java/com/cybersoft/capstone/entity/Orders.java
+++ springboot/src/main/java/com/cybersoft/capstone/entity/Orders.java
@@ -5,8 +5,13 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Data;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.FetchType;
+import java.util.List;
 
 import java.io.Serializable;
+import java.util.Date;
 
 @Entity
 @Data
@@ -14,4 +19,9 @@ public class Orders implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    private Date orderDate;
+    private String status;
+    private double totalAmount;
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<OrderItem> orderItems;
 }
