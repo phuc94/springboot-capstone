d39fd6fb1f2d04e9954c370d963beba2f5c00bd1
nghiadeveloper <hieunghia.dev@gmail.com>
2025-05-08 18:37:39 +0700

    Hoàn thành task SBC-80

--- springboot/src/main/java/com/cybersoft/capstone/controller/admin/AdminGameController.java
+++ springboot/src/main/java/com/cybersoft/capstone/controller/admin/AdminGameController.java
@@ -1,12 +1,45 @@
 package com.cybersoft.capstone.controller.admin;
 
-//import com.cybersoft.capstone.payload.response.BaseResponse;
-//import io.swagger.v3.oas.annotations.Parameter;
-//import io.swagger.v3.oas.annotations.enums.ParameterIn;
-//import org.springframework.web.bind.annotation.PathVariable;
-//
-//public class AdminGameController {
-//    default BaseResponse<Object> deleteAdminGame(@Parameter(name = "id",description = "Game id to be delete",required = true,in = ParameterIn.PATH) @PathVariable("id") Integer id) {
-//        return this.getDelegate().deleteAdminGame(id);
-//    }
-//}
+import com.cybersoft.capstone.dto.AdminGameDTO;
+import com.cybersoft.capstone.service.interfaces.AdminGameService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/game")
+public class AdminGameController {
+
+    private final AdminGameService adminGameService;
+
+    public AdminGameController(AdminGameService adminGameService) {
+        this.adminGameService = adminGameService;
+    }
+
+    @GetMapping
+    public List<AdminGameDTO> getAllGames() {
+        return adminGameService.getAllAdminGames();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<AdminGameDTO> getGameById(@PathVariable int id) {
+        return ResponseEntity.ok(adminGameService.getAdminGameById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<AdminGameDTO> createGame(@RequestBody AdminGameDTO adminGameDTO) {
+        return ResponseEntity.ok(adminGameService.createAdminGame(adminGameDTO));
+    }
+
+    @PostMapping("/{id}")
+    public ResponseEntity<AdminGameDTO> updateGame(@PathVariable int id, @RequestBody AdminGameDTO adminGameDTO) {
+        return ResponseEntity.ok(adminGameService.updateAdminGame(id, adminGameDTO));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteGameById(@PathVariable int id) {
+        adminGameService.deleteAdminGameById(id);
+        return ResponseEntity.noContent().build();
+    }
+}
--- springboot/src/main/java/com/cybersoft/capstone/dto/mapper/GameMapper.java
+++ springboot/src/main/java/com/cybersoft/capstone/dto/mapper/GameMapper.java
@@ -1,11 +1,20 @@
 package com.cybersoft.capstone.dto.mapper;
 
 import com.cybersoft.capstone.dto.AdminGameDTO;
+import com.cybersoft.capstone.entity.GameDescription;
 import com.cybersoft.capstone.entity.Games;
+import com.cybersoft.capstone.entity.Platforms;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface GameMapper {
-    Games toGames(AdminGameDTO gameDTO);
+
+    @Mapping(source = "gameDTO.id", target = "id")
+    @Mapping(source = "gameDTO.title", target = "title")
+    Games toGames(AdminGameDTO gameDTO, Platforms platform, GameDescription gameDescription);
+
+    @Mapping(source = "platform.id", target = "platformId")
+    @Mapping(source = "gameDescription.id", target = "descriptionId")
     AdminGameDTO toAdminGameDTO(Games games);
 }
