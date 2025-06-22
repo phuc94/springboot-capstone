8d18f248973ab125862f2b66edaa880928ed8e6a
Phuc <trungphuc94@gmail.com>
2025-06-22 18:26:38 +0000

    fix lazy img

--- frontstore/src/components/Description/index.tsx
+++ frontstore/src/components/Description/index.tsx
@@ -3,6 +3,7 @@ import { useEffect } from "react";
 
 const Description = ({data}: {data: string}) => {
 
+  // script for lazy load muga.me data
   useEffect(()=>{
     if (document.querySelector('frame#frame-loaded')) { return; }
     const divEl = document.querySelector('div.rll-youtube-player') as HTMLElement
@@ -17,6 +18,16 @@ const Description = ({data}: {data: string}) => {
       iframe.allow = 'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
       divEl.appendChild(iframe)
     }
+
+    const pictureEls = document.querySelectorAll('picture.aligncenter.size-full') as NodeList
+    pictureEls.forEach((el: any) => {
+      el.childNodes[1].src = el.childNodes[1].dataset.lazySrc
+      el.childNodes[1].style.width = '100%'
+      el.childNodes[1].style.aspectRatio = 'auto'
+      el.childNodes[1].style.height = 'auto'
+      console.log(el.childNodes[1].dataset.lazySrc)
+    })
+
   }, [data])
 
   return <Container>
